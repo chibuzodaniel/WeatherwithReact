4b6335fbccacbeb8a1967cc93a6dce020ba270e5
chibuzodaniel <chibuzodaniel71@gmail.com>
2023-06-24 17:03:35 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import axios from 'axios';
+
 
 function App() {
+  const [data, setData] =useState({})
+  const [location, setLocation] =useState('')
+  const url = `https://api.openweathermap.org/data/2.5/weather?${location}&appid=952e2b0dbe712c5b57a6aaabeea68dbf`
+  
+  const searchLocation=(event)=>{
+    if (event.key ==='Enter'){
+      axios.get(url).then((response)=>{
+        setData(response.data)
+        console.log(response.data)
+      })
+    }
+  
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app"> 
+    <div className="container">
+      <div className="top">
+        <div className="location">
+          <p>Enugu</p>
+        </div>
+        <div className="temp">
+          <h1>60°F</h1>
+        </div>
+        <div className="description">
+          <p>Clouds</p>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="feels">
+          <p className='bold'>60°F</p>
+          <p>Feels like</p>
+        </div>
+        <div className="humidity">
+          <p className='bold'>50%</p>
+          <p>Humidity</p>
+        </div>
+        <div className="wind">
+         <p className='bold'>12 MPH</p>
+         <p>Wind Speed</p>
+        </div>
+      </div>
+      
+      </div> 
     </div>
   );
 }
